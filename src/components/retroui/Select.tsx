bdaac92458e11e4eb00a08187ac56e5d0dcc0afa
Ulgamon <ilicjustin@gmail.com>
2025-09-03 15:03:12 +0200

    deleted unused element

--- src/components/retroui/Select.tsx
+++ src/components/retroui/Select.tsx
@@ -3,7 +3,6 @@
 import { cn } from "@/lib/utils";
 import * as SelectPrimitive from "@radix-ui/react-select";
 import { Check, ChevronDown, ChevronUp } from "lucide-react";
-import React from "react";
 
 const Select = SelectPrimitive.Root;
 
@@ -16,7 +15,7 @@ const SelectTrigger = ({
     <SelectPrimitive.Trigger
       className={cn(
         "flex h-10 min-w-40 items-center shadow-md justify-between border-2 border-input border-border bg-transparent px-4 py-2 placeholder:text-muted-foreground outline-none focus:outline-none focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
-        className,
+        className
       )}
       {...props}
     >
@@ -45,7 +44,7 @@ const SelectContent = ({
           "relative z-50 min-w-[8rem] overflow-hidden border border-border bg-background text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
           position === "popper" &&
             "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
-          className,
+          className
         )}
         position={position}
         {...props}
@@ -56,7 +55,7 @@ const SelectContent = ({
         <SelectPrimitive.Viewport
           className={cn(
             position === "popper" &&
-              "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]",
+              "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
           )}
         >
           {children}
@@ -79,7 +78,7 @@ const SelectItem = ({
   <SelectPrimitive.Item
     className={cn(
       "relative flex w-full cursor-default select-none items-center py-1.5 px-2 outline-none data-[highlighted]:bg-primary data-[highlighted]:text-primary-foreground focus:bg-primary focus:text-primary-foreground data-disabled:pointer-events-none data-disabled:opacity-50",
-      className,
+      className
     )}
     {...props}
   >
