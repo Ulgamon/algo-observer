9a4c15db1825ea1b325d7703fc900230d953100e
Ulgamon <ilicjustin@gmail.com>
2025-09-04 19:09:31 +0200

    created components for showing sidebar list

--- src/components/SidebarElement.tsx
+++ src/components/SidebarElement.tsx
@@ -1,9 +1,10 @@
-export const SidebarElement: React.FC<React.PropsWithChildren> = ({
-  children,
-}) => {
+import { Link } from "react-router";
+import type { SidebarLinkData } from "../lib/data";
+
+export const SidebarElement = ({ name, to }: SidebarLinkData) => {
   return (
     <li className="relative ps-4 flex cursor-default select-none items-center rounded-xs px-2 py-1.5 text-sm outline-hidden transition-colors hover:bg-primary hover:text-black focus:bg-primary data-disabled:pointer-events-none data-disabled:opacity-50">
-      {children}
+      <Link to={to}>{name}</Link>
     </li>
   );
 };
