096aa23ba6f01aa3ce3643b324e42866b4bd4b2b
Ulgamon <ilicjustin@gmail.com>
2025-09-05 10:32:06 +0200

    themecontext test written

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,2 +1,17 @@
 import "@testing-library/jest-dom/vitest";
 import "@testing-library/user-event";
+
+// Mock for functiomatchMedia because vitest doesn't support it
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
